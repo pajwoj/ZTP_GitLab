e198d3d8fe6e2284415003050ed24687678211f7
Wojciech Pajtel <92268966+pajwoj@users.noreply.github.com>
2024-03-12 15:18:13 +0100

    add Spring Initializr files

---
+++ src/main/java/pl/pajwoj/ztp/ZtpApplication.java
@@ -0,0 +1,13 @@
+package pl.pajwoj.ztp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pajwoj/ztp/ZtpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pajwoj.ztp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
